92b1e3f177a727fe1cc0545b6950913f2603c6a9
JeffreyDeras <jefreyjb10@gmail.com>
2023-05-30 17:10:17 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    id: 1,
+    title: 'Novo FIlme',
+    description: 'ola',
+    photoCover:
+      'https://ovicio.com.br/wp-content/uploads/2022/07/20220721-20220721_202929.jpg'
+  },
+  {
+    id: 2,
+    title: 'saiu novo filme',
+    description: 'ola',
+    photoCover: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2e03dbad-1afa-4282-8411-d813fd363226/d5sorwu-87a2ac2e-3bf4-42df-91cf-4d90bb64c8cd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJlMDNkYmFkLTFhZmEtNDI4Mi04NDExLWQ4MTNmZDM2MzIyNlwvZDVzb3J3dS04N2EyYWMyZS0zYmY0LTQyZGYtOTFjZi00ZDkwYmI2NGM4Y2QuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.a5SnxWpJW7sW3UqDo_X9BkuRmzE5Pz4OlwaDdA_lOvc'
+  },
+];
